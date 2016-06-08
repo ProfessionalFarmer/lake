defbd649eed7a9f82c78a8f7958d51e4da24f4f6
Jason Zhu <sxzhuxu@126.com>
2016-06-08 15:09:47 +0800

    delete a character

--- new.install.sh
+++ new.install.sh
@@ -11,8 +11,7 @@ apt-get install tree
 
 # icdiff is more useful compared with diff. icdiff 的可视效果比diff好，不过比较大文件时，效率不高
 # https://www.jefftk.com/icdiff
-curl -s https://raw.githubusercontent.com/jeffkaufman/icdiff/release-1.8.1/icdiff | sudo tee /usr/local/bin/icdiff > /dev/null \
-  && sudo chmod ugo+rx /usr/local/bin/icdiff
+curl -s https://raw.githubusercontent.com/jeffkaufman/icdiff/release-1.8.1/icdiff | sudo tee /usr/local/bin/icdiff > /dev/null && sudo chmod ugo+rx /usr/local/bin/icdiff
 
 
 
