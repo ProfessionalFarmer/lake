f8ec53aaef120bece904b894d484aed2e8fca0e4
Jason <sxzhuxu@126.com>
2017-04-27 14:39:03 +0800

    delete

--- matchSelect.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# Please note The first column must be considered.
-# This script will print information in $3
-
-cat $1 | cut -f $2 | awk -F "\t" '{ print $0 }' > .temp.1.select
-cat $3 | grep -Ff .temp.1.select | cut -f $4
-rm .temp.1.select
-
-
