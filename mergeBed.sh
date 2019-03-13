28c509e2ab78f73aa82fd6679c55e5e78d0f2eec
Jason <sxzhuxu@126.com>
2019-03-13 16:51:09 +0800

    t

--- mergeBed.sh
+++ mergeBed.sh
@@ -2,7 +2,7 @@
 # 2018-01-06: change $1 to $@ variable
 bedtools='/share/apps/bedtools2/bin/bedtools'
 
-cat $@ | sort -k1,1 -k2,2n > bedtools.in.sorted.bed
+cat $@ | sort -V -k1,1 -k2,2n > bedtools.in.sorted.bed
 
 # -o can be followed by distinct, sum, min, max, absmin, absmax, mean, median, collapse, count_distinct, count
 $bedtools merge -i bedtools.in.sorted.bed -c 4 -o distinct
