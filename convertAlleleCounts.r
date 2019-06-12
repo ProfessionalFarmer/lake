84599be9e673c50030bd7fee719b46fd5558dbb7
Zhongxu <sxzhuxu@126.com>
2019-06-12 09:15:18 +0800

    convert allelecount dada

--- convertAlleleCounts.r
+++ convertAlleleCounts.r
@@ -1,19 +1,25 @@
 #!/bin/env Rscript
-# Description:
-# R-script for converting output from AlleleCount to BAF and LogR values.
-#
-# Input:
-# AlleleCounter output file for tumor and normal samples
-# The first line should contain a header describing the data
-# The following columns and headers should be present:
-# CHR    POS     Count_A Count_C Count_G Count_T Good_depth
-#
-# Output:
-# BAF and LogR tables (tab delimited text files)
-# Clone From: https://github.com/SciLifeLab/Sarek/blob/master/bin/convertAlleleCounts.r
-################################################################################
 
-##First read in the arguments listed at the command line
+#################################################################################
+### Jose Espinosa-Carrasco AGENDAS-IRB Group. Nov 2018                        ###
+#################################################################################
+### Adapted from:                                                             ###
+### https://github.com/SciLifeLab/Sarek/blob/master/bin/convertAlleleCounts.r ###
+###                                                                           ###
+### R-script for converting output from AlleleCount to BAF and LogR values.   ###
+###                                                                           ###
+### Input:                                                                    ###
+### AlleleCounter output file for tumor and normal samples                    ###
+### The first line should contain a header describing the data                ###
+### The following columns and headers should be present:                      ###
+### CHR    POS     Count_A Count_C Count_G Count_T Good_depth                 ###
+###                                                                           ###
+### Output:                                                                   ###
+### BAF and LogR tables (tab delimited text files)
+### Clone From: https://github.com/daormar/bio-panpipe Thanks
+#################################################################################
+
+## Read command line arguments
 args = commandArgs(trailingOnly=TRUE)
 
 ## args is now a list of character vectors
@@ -21,74 +27,78 @@ args = commandArgs(trailingOnly=TRUE)
 if(length(args)<5){
     stop("No input files supplied\n\nUsage:\nRscript convertAlleleCounts.r tumorid tumorac normalid normalac gender\nWhere:\ntumorid - id of tumor sample\ntumorac - output from AlleleCount for the tumor\nnormalid - id of normal sample\nnormalac - output from AlleleCount for the normal\ngender - XX or XY\n\n")
 } else{
-    tumorid = args[1]
-    tumorac = args[2]
-    normalid = args[3]
-    normalac = args[4]
+    tumor_id = args[1]
+    tumor_ac = args[2]
+    normal_id = args[3]
+    normal_ac = args[4]
     gender = args[5]
+    out_dir = args[6]
 }
 
-tumorcounts = read.table(tumorac, header=F, sep="\t")
-normalcounts = read.table(normalac, header=F, sep="\t")
+tumor_counts <- read.table(tumor_ac, header=F, skip=1, sep="\t")
+normal_counts <- read.table(normal_ac, header=F, skip=1, sep="\t")
 
-SNPpos = matrix(nrow = dim(normalcounts)[1],ncol = 2)
+SNP_pos <- matrix(nrow = dim(normal_counts)[1], ncol = 2)
 
-rownames(SNPpos) = paste("snp",1:dim(SNPpos)[1],sep="")
+rownames(SNP_pos) <- paste("snp", 1:dim(SNP_pos)[1], sep="")
 
 #Change rownames to "chr_pos" instead, such as 1_44552
 #This does not exactly work:
-#rownames(SNPpos) = apply(cbind(tumorcounts[,1], tumorcounts[,2]), 1, paste, collapse="_")
+#rownames(SNP_pos) <- apply(cbind(tumor_counts[,1], tumor_counts[,2]), 1, paste, collapse="_")
 #This is for compatibility with gc correction file
 
-colnames(SNPpos) = c("Chr","Position")
-SNPpos[,1] = as.vector(normalcounts[,1])
-SNPpos[,2] = normalcounts[,2]
+colnames(SNP_pos) <- c("Chr", "Position")
+SNP_pos[,1] <- as.vector(normal_counts[,1])
+SNP_pos[,2] <- normal_counts[,2]
 
-#Caclulate BAF
-Tumor_BAF = matrix(nrow = dim(normalcounts)[1],ncol = 1)
-rownames(Tumor_BAF) = rownames(SNPpos)
-colnames(Tumor_BAF) = c(tumorid)
-acgt = tumorcounts[,c(3:6)]
-acgts = t(apply(acgt,1,sort))
-Tumor_BAF[,1] = acgts[,4]/(acgts[,3]+acgts[,4])
-Tumor_BAF[,1] = ifelse(runif(length(Tumor_BAF[,1]))<0.5,Tumor_BAF[,1],1-Tumor_BAF[,1])
-Tumor_BAF[is.nan(Tumor_BAF)]=NA
+#Calculate BAF
+tumor_BAF <- matrix(nrow = dim(normal_counts)[1], ncol = 1)
+rownames(tumor_BAF) <- rownames(SNP_pos)
+colnames(tumor_BAF) <- c(tumor_id)
+acgt <- tumor_counts[ ,c(3:6)]
+acgts <- t(apply(acgt, 1, sort))
+tumor_BAF[,1] <- acgts[,4] / (acgts[,3] + acgts[,4])
+tumor_BAF[,1] <- ifelse(runif(length(tumor_BAF[,1])) < 0.5, tumor_BAF[,1], 1 - tumor_BAF[,1])
+tumor_BAF[is.nan(tumor_BAF)] <- NA
 
-Germline_BAF = matrix(nrow = dim(normalcounts)[1],ncol = 1)
-rownames(Germline_BAF) = rownames(SNPpos)
-colnames(Germline_BAF) = c(normalid)
-acgt = normalcounts[,c(3:6)]
-acgts = t(apply(acgt,1,sort))
-Germline_BAF[,1] = acgts[,4]/(acgts[,3]+acgts[,4])
-Germline_BAF[,1] = ifelse(runif(length(Germline_BAF[,1]))<0.5,Germline_BAF[,1],1-Germline_BAF[,1])
-Germline_BAF[is.nan(Germline_BAF)]=NA
+germline_BAF <- matrix(nrow = dim(normal_counts)[1], ncol = 1)
+rownames(germline_BAF) <- rownames(SNP_pos)
+colnames(germline_BAF) <- c(normal_id)
+acgt <- normal_counts[,c(3:6)]
+acgts <- t(apply(acgt, 1, sort))
+germline_BAF[,1] <- acgts[,4] / (acgts[,3] + acgts[,4])
+germline_BAF[,1] <- ifelse(runif(length(germline_BAF[,1])) < 0.5, germline_BAF[,1], 1 - germline_BAF[,1])
+germline_BAF[is.nan(germline_BAF)] <- NA
 
+tumor_LogR <- matrix(nrow = dim(normal_counts)[1], ncol = 1)
+germline_LogR <- matrix(nrow = dim(normal_counts)[1], ncol = 1)
+rownames(tumor_LogR) <- rownames(SNP_pos)
+colnames(tumor_LogR) <- c(tumor_id)
+rownames(germline_LogR) <- rownames(SNP_pos)
+colnames(germline_LogR) <- c(normal_id)
+tumor_LogR[,1] <- log(tumor_counts[,7] / normal_counts[,7], 2)
+germline_LogR[,1] <- 0
+tumor_LogR[is.infinite(tumor_LogR)] <- NA
 
-Tumor_LogR = matrix(nrow = dim(normalcounts)[1],ncol = 1)
-Germline_LogR = matrix(nrow = dim(normalcounts)[1],ncol = 1)
-rownames(Tumor_LogR) = rownames(SNPpos)
-colnames(Tumor_LogR) = c(tumorid)
-rownames(Germline_LogR) = rownames(SNPpos)
-colnames(Germline_LogR) = c(normalid)
-Tumor_LogR[,1] = log(tumorcounts[,7]/normalcounts[,7],2)
-Germline_LogR[,1] = 0
-Tumor_LogR[is.infinite(Tumor_LogR)]=NA
-if(gender=="XY") {
-    Tumor_LogR[SNPpos[,1]=="X",1] = Tumor_LogR[SNPpos[,1]=="X",1]-1
-    Germline_LogR[SNPpos[,1]=="X",1] = Germline_LogR[SNPpos[,1]=="X",1]-1
+if (gender=="XY") {
+    tumor_LogR[SNP_pos[,1] == "X", 1] <- tumor_LogR[SNP_pos[,1]=="X", 1] - 1
+    germline_LogR[SNP_pos[,1] == "X", 1] <- germline_LogR[SNP_pos[,1] == "X", 1] - 1
 }
-Tumor_LogR[,1] = Tumor_LogR[,1] - median(Tumor_LogR[,1],na.rm=T)
+tumor_LogR[,1] <- tumor_LogR[ ,1] - median(tumor_LogR[ ,1], na.rm=T)
 # set regions with 0 reads in tumor and normal to a LogR of 0.
-Tumor_LogR[is.na(Tumor_LogR[,1]),1] = 0
+tumor_LogR[is.na(tumor_LogR[ ,1]), 1] <- 0
 
 # limit the number of digits:
-Tumor_LogR = round(Tumor_LogR,4)
-Tumor_BAF = round(Tumor_BAF,4)
-Germline_LogR = round(Germline_LogR,4)
-Germline_BAF = round(Germline_BAF,4)
+tumor_LogR <- round(tumor_LogR,4)
+tumor_BAF <- round(tumor_BAF,4)
+germline_LogR <- round(germline_LogR,4)
+germline_BAF <- round(germline_BAF,4)
+
+## Making sure that the chromosomes do not include the label "chr" in their id
+SNP_pos <- gsub("chr", "", SNP_pos)
 
 # write output to files
-write.table(cbind(SNPpos,Tumor_LogR),paste(tumorid,".LogR",sep=""),sep="\t",row.names=T,col.names=NA,quote=F)
-write.table(cbind(SNPpos,Tumor_BAF),paste(tumorid,".BAF",sep=""),sep="\t",row.names=T,col.names=NA,quote=F)
-write.table(cbind(SNPpos,Germline_LogR),paste(normalid,".LogR",sep=""),sep="\t",row.names=T,col.names=NA,quote=F)
-write.table(cbind(SNPpos,Germline_BAF),paste(normalid,".BAF",sep=""),sep="\t",row.names=T,col.names=NA,quote=F)
+write.table(cbind (SNP_pos, tumor_LogR), paste(out_dir,"/", tumor_id, ".LogR", sep=""), sep="\t", row.names=T, col.names=NA, quote=F)
+write.table(cbind (SNP_pos, tumor_BAF), paste(out_dir, "/", tumor_id, ".BAF", sep=""), sep="\t", row.names=T, col.names=NA, quote=F)
+write.table(cbind (SNP_pos, germline_LogR), paste(out_dir,"/", normal_id, ".LogR", sep=""), sep="\t", row.names=T, col.names=NA, quote=F)
+write.table(cbind (SNP_pos, germline_BAF), paste(out_dir, "/", normal_id, ".BAF", sep=""), sep="\t", row.names=T, col.names=NA, quote=F)
